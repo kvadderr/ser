19e372b26caae676610881229a372802c6898c67
Bakytbek <93685132+kvadderr@users.noreply.github.com>
2022-03-16 14:54:20 +0600

    first commit

---
+++ api/survey/router.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const {
+    newSurvey,
+    getSurvey,
+    updateSurvey,
+    getAnswer,
+    setResult
+} = require("./controller");
+
+router.patch("/", updateSurvey);
+router.post("/", newSurvey);
+router.post("/result", setResult);
+router.get("/role/:role/id_user/:id_user/id_group/:id_group", getSurvey);
+router.get("/id_survey/:id_survey", getAnswer);
+
+module.exports = router;
